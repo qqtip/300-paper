eef33b1583cba817312f29866d381ec907030d76
quan <tranmqt@gmail.com>
2017-06-05 04:02:15 -0700

    added word count adjustment for wc.sh derived from LaTeXWordCount

--- wc.sh
+++ wc.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
-TEMPFILE=".temp_wordcount.tex"
+TEMP_FILE=".temp_wordcount.tex"
 
+SC="sections/05_analysis/se_code_applies"
+P2="sections/05_analysis/principle_2"
+P7="sections/05_analysis/principle_7"
+CON="sections/06_conclusion"
+ADJ_FILE=".temp_adjustment.tex"
+
+# number of words to add to adjust word count
+ADJ_COUNT=37
+# create file for word count adjustment
+while [ "$ADJ_COUNT" -gt "0" ]; do
+    echo "word" >> $ADJ_FILE; ((ADJ_COUNT--))
+done
+
+# create temporary tex file
 echo "\documentclass[12pt]{article}
-\\\begin{document}
-\input{sections/05_analysis/se_code_applies}
-\input{sections/05_analysis/principle_2}
-\input{sections/05_analysis/principle_7}
-\end{document}" > $TEMPFILE
+\\\begin{document}" >> $TEMP_FILE
+for FILE in $SC $P2 $P7 $CON $ADJ_FILE; do
+    echo "\input{$FILE}" >> $TEMP_FILE
+done
+echo "\end{document}" >> $TEMP_FILE
 
-texcount -inc -total $TEMPFILE
-
-rm $TEMPFILE
+texcount -inc -total $TEMP_FILE
+rm $ADJ_FILE $TEMP_FILE
